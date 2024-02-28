79050c4e15c7aba35370db80757f1d96bd403aa5
root <root@debian.42yerevan.am>
2024-02-28 13:19:41 -0500

    started

---
+++ make_directories.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+mkdir project
+mkdir project/srcs
+touch project/Makefile
+mkdir project/srcs/requirements
+touch project/srcs/docker-compose.yml
+touch project/srcs/.env
+echo "DB_NAME=wordpress" >> project/srcs/.env
+echo "DB_ROOT=rootpass" >> project/srcs/.env
+echo "DB_USER=wpuser" >> project/srcs/.env
+echo "DB_PASS=wppass" >> project/srcs/.env
+mkdir project/srcs/requirements/bonus
+mkdir project/srcs/requirements/mariadb
+mkdir project/srcs/requirements/mariadb/conf
+touch project/srcs/requirements/mariadb/conf/create_db.sh
+mkdir project/srcs/requirements/mariadb/tools
+touch project/srcs/requirements/mariadb/tools/.gitkeep
+echo "" > project/srcs/requirements/mariadb/tools/.gitkeep
+touch project/srcs/requirements/mariadb/Dockerfile
+touch project/srcs/requirements/mariadb/.dockerignore
+echo ".git" > project/srcs/requirements/mariadb/.dockerignore
+echo ".env" >> project/srcs/requirements/mariadb/.dockerignore
+mkdir project/srcs/requirements/nginx
+mkdir project/srcs/requirements/nginx/conf
+touch project/srcs/requirements/nginx/conf/nginx.conf
+mkdir project/srcs/requirements/nginx/tools
+touch project/srcs/requirements/nginx/Dockerfile
+echo ".git" > project/srcs/requirements/mariadb/.dockerignore
+echo ".env" >> project/srcs/requirements/mariadb/.dockerignore
+mkdir project/srcs/requirements/tools
+mkdir project/srcs/requirements/wordpress
+mkdir project/srcs/requirements/wordpress/conf
+touch project/srcs/requirements/wordpress/conf/wp-config-create.sh
+mkdir project/srcs/requirements/wordpress/tools
+touch project/srcs/requirements/wordpress/tools/.gitkeep
+echo "" > project/srcs/requirements/wordpress/tools/.gitkeep
+touch project/srcs/requirements/wordpress/Dockerfile
+touch project/srcs/requirements/wordpress/.dockerignore
+echo ".git" > project/srcs/requirements/wordpress/.dockerignore
+echo ".env" >> project/srcs/requirements/wordpress/.dockerignore
